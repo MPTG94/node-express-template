a9932425473ac82731836f5ca2d4390cd7e95ff8
Mor Paz <1912.mor@gmail.com>
2016-09-30 13:51:13 +0300

    added gulpfile Added basic html page Added static files loading from html path Added path module

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 'use strict';
 // Adding express module
 var express = require('express');
+var path = require('path');
 var app = express();
+// Setting var for html files path
+var htmlDir = './app/html/';
 
 // Setting default route for root
+// Serving index.html for root request
+app.use(express.static(path.join(__dirname, '/html')));
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendFile(htmlDir + 'index.html');
 });
 
 // Setting app listen on port 3000
