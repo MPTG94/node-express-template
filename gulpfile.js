1e13168023166a81a9a10c91b530362e9fef79b3
Mor Paz <1912.mor@gmail.com>
2016-09-30 14:42:13 +0300

    Added browsersync integration Updated index.html page

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 'use strtict';
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000"
+  });
+});
+
+gulp.task('default', ['browser-sync'], function() {
+  gulp.watch(["./app/html/*.html"], reload);
+});
+
+gulp.task('nodemon', function(cb) {
+  var callbackCalled = false;
+  return nodemon({
+    script: 'app/index.js'
+  }).on('start', function() {
+    if (!callbackCalled) {
+      callbackCalled = true;
+      cb();
     }
   });
 });
