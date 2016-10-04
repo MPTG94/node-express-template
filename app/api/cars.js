d60acdb85b8267e26acace032ef96e33306aa160
Mor Paz <1912.mor@gmail.com>
2016-10-04 05:14:49 +0300

    Updated JSHint to use ES6 Added POST Methods to create a car and a company Added body-parser module

--- app/api/cars.js
+++ app/api/cars.js
@@ -1,16 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var connection = require('../db/dbConnection');
+
+// Create parsers
+// create application/json parser
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true
+});
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
-// define the home page route
-// define the home page route
+
+// define the basic get route
 router.get('/', function(req, res) {
-  connection.query('SELECT * FROM cardb.companies', function(err, rows, fields) {
+  connection.query('SELECT * FROM cardb.cars', function(err, rows, fields) {
     if (err) {
       throw err;
     } else {
@@ -19,9 +28,39 @@ router.get('/', function(req, res) {
   });
 });
 
-// define the about route
-router.post('/', function(req, res) {
-  res.send('POST All Cars');
+// define the create new car route
+router.post('/create', jsonParser, function(req, res) {
+  if (!req.body) {
+    // No body in request, returning 400 status code
+    return res.sendStatus(400);
+  }
+  var car = req.body;
+  if (car.hasOwnProperty('Name') && car.hasOwnProperty('Trim') &&
+    car.hasOwnProperty('HorsePower') && car.hasOwnProperty('CompanyID') &&
+    Object.keys(car).length === 4) {
+    // request body has all necessary values
+    var query = connection.query(`INSERT INTO cardb.cars SET ?`, car,
+      function(err, result) {
+        if (err) {
+          throw err;
+        } else {
+          connection.query('SELECT * FROM cardb.cars WHERE ID = ?',
+            result.insertId,
+            function(err, rows, fields) {
+              if (err) {
+                throw err;
+              } else {
+                res.send(rows);
+              }
+            });
+        }
+      });
+  } else {
+    // The input object either doesn't have all the necessary keys
+    // or it has more keys than required
+    return res.sendStatus(400);
+  }
+  // console.log(query.sql);
 });
 
 module.exports = router;
--- app/api/companies.js
+++ app/api/companies.js
@@ -1,15 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var connection = require('../db/dbConnection');
+
+var app = express();
+
+// Create parsers
+// create application/json parser
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
-// define the home page route
+// define the basic get route
 router.get('/', function(req, res) {
-  connection.query('SELECT * FROM cardb.cars', function(err, rows, fields) {
+  connection.query('SELECT * FROM cardb.companies', function(err, rows, fields) {
     if (err) {
       throw err;
     } else {
@@ -18,9 +29,38 @@ router.get('/', function(req, res) {
   });
 });
 
-// define the about route
-router.post('/', function(req, res) {
-  res.send('POST All Companies');
+// define the create new company route
+router.post('/create', jsonParser, function(req, res) {
+  if (!req.body) {
+    // No body in request, returning 400 status code
+    return res.sendStatus(400);
+  }
+  var company = req.body;
+  if (company.hasOwnProperty('Name') && company.hasOwnProperty('Established') &&
+    Object.keys(company).length === 2) {
+    // request body has all necessary values
+    var query = connection.query(`INSERT INTO cardb.companies SET ?`, company,
+      function(err, result) {
+        if (err) {
+          throw err;
+        } else {
+          connection.query('SELECT * FROM cardb.companies WHERE ID = ?',
+            result.insertId,
+            function(err, rows, fields) {
+              if (err) {
+                throw err;
+              } else {
+                res.send(rows);
+              }
+            });
+        }
+      });
+  } else {
+    // The input object either doesn't have all the necessary keys
+    // or it has more keys than required
+    return res.sendStatus(400);
+  }
+  // console.log(query.sql);
 });
 
 module.exports = router;
